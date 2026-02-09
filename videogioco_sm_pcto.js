14427400ebe57e936f23b54f92932cbd3e2c78ef
Simone <simone.foi@itiscuneo.edu.it>
2026-02-09 12:02:32 +0100

    commit 1

---
+++ videogioco_sm_pcto.js
@@ -0,0 +1,51 @@
+let player;
+let pista;
+
+let schemaAttuale;
+let oldSchema;
+
+let start_image;
+let pause_image;
+let tutorial_image;
+let game_over;
+
+function preload(){
+    //player = loadImage();
+    //pista = loadImage();
+    //schemaAttuale = loadImage();
+    //oldSchema = loadImage();
+    //start_image = loadImage();
+    //pause_image = loadImage();
+    //tutorial_image = loadImage();
+    //game_over = loadImage();
+}
+
+function setup(){
+    createCanvas(start_image.width, start_image.height);
+    frameRate(50);
+
+    schemaAttuale = -2;
+    oldSchema = schemaAttuale;
+}
+
+function draw(){
+
+}
+
+function musica(){
+
+}
+
+function stopAllMusic(){
+
+}
+
+function mouseClicked() {
+
+}
+
+
+function keyPressed(){
+
+}
+
